07634e3c37cc8996edf3147b53efc75b6362fece
michaelguedj <39969803+michaelguedj@users.noreply.github.com>
2019-11-18 14:23:30 +0100

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,34 @@
+==============
+   README 
+==============
+
+ Name: sharpdoc
+
+ Author: Dr Michael GUEDJ
+ 
+ Description: 
+   
+   Usage: sharpdoc <some_file>
+   --> generates a "README.txt" file containing
+       the part of <some_file> beginning with "##"
+   
+
+ Version: 0.3
+
+ Date: 18/11/2019
+
+ License: MIT LICENSE 
+ -- see the file MIT-LICENSE.txt
+
+
+
+ Install 
+-----------
+___$ bash INSTALL.sh
+
+
+
+ Uninstall 
+-----------
+___$ bash ~/.123/<NAME>/UNINSTALL.sh
+
---
+++ sharpdoc.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+##
+## Name: sharpdoc
+##
+## Author: Dr Michael GUEDJ
+## 
+## Description: 
+##   
+##   Usage: sharpdoc <some_file>
+##   --> generates a "README.txt" file containing
+##       the part of <some_file> beginning with "##"
+##   
+##
+## Version: 0.3
+##
+## Date: 18/11/2019
+##
+## License: MIT LICENSE 
+## -- see the file MIT-LICENSE.txt
+##
+
+function usage 
+{
+	echo "Usage: sharpdoc <some_file>"
+	echo "--> generates a \"README.txt\" file containing"
+	echo "    the part of <some_file> beginning with \"##\""
+	exit 1
+}
+
+
+if [ $# -lt 1 ]
+then 
+	echo "!: 1 argument is required."
+	usage
+fi
+
+if [ ! -e $1 ]
+then 
+	echo "!: The given argument must be in the current directory."
+	usage
+fi
+
+if [ -d $1 ]
+then 
+	echo "!: The given argument can't be a directory."
+	usage
+fi
+
+
+echo "==============" > README.txt
+echo "   README " >> README.txt
+echo "==============" >> README.txt
+
+cat $1 | grep "^##" | sed "s/^##//" >> README.txt
+
+
+echo -e "\n\n Install " >> README.txt
+echo -e "-----------" >> README.txt
+echo -e "___$ bash INSTALL.sh\n" >> README.txt
+
+
+
+echo -e "\n\n Uninstall " >> README.txt
+echo -e "-----------" >> README.txt
+echo -e "___$ bash ~/.123/sharpdoc/UNINSTALL.sh\n" >> README.txt
+
